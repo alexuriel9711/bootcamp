8e246ebe4d78b459c975028041373dee407b2a0f
alexuriel9711 <antixalex@gmail.com>
2024-05-08 12:22:32 -0500

    module_3 add promises js

--- module_III/lab3/lab3.js
+++ module_III/lab3/lab3.js
@@ -5,35 +5,83 @@
  */
 
 
-let productName = "";
-let productType = "";
-let quantity = "";
-let price = "";
-let latitude = "";
-let longitude = "";
+let productName = null;
+let productType = null;
+let quantity = null;
+let price = null;
+let latitude = null;
+let longitude = null;
 let product = {};
 
+const getPromiseProductDataForRequest = () => {; 
+  return new Promise((resolve, reject) => {
+    const productName = document.getElementById("productName").value;
+    const productType = document.getElementById("productType").value;
+    const quantity = document.getElementById("quantity").value;
+    const price = document.getElementById("price").value;
+    const latitude = document.getElementById("latitude").value;
+    const longitude = document.getElementById("longitude").value;
+    resolve({
+      productName: productName,
+      productType: productType,
+      quantity: quantity,
+      price: price,
+      latitude: latitude,
+      longitude: longitude
+    });
+  });
+}
+const promiseForSanitizeData = (data) => {
+  return new Promise((resolve, reject) => {
+    productName = data.productName;
+    productType = data.productType;
+    quantity = data.quantity;
+    price = data.price;
+    latitude = data.latitude;
+    longitude = data.longitude;
+    resolve();
+  });
+};
+const buildData = data => {
+  let buildData = null;
 
-const getProductDataForm = () => {
-    productName = document.getElementById("productName").value;
-    productType = document.getElementById("productType").value;
-    quantity = document.getElementById("quantity").value;
-    price = document.getElementById("price").value;
-    latitude = document.getElementById("latitude").value;
-    longitude = document.getElementById("longitude").value;
+  if (data !== null && data !== undefined) {
+    buildData = {
+      productName: data.productName,
+      productType: data.productType,
+      quantity: data.quantity,
+      price: data.price,
+      latitude: data.latitude,
+      longitude: data.longitude
+    }
+  }
+  
+  return buildData;
+};
+const promiseBuildData = () =>{
+  return new Promise((resolve, reject) => {
+    product = buildData ({productName: productName, productType: productType, quantity: quantity, price: price, latitude: latitude, longitude: longitude});
+    resolve(product);
+  });
 };
 
 
-const buildProductDataForRequest = () => {
-    product = {
-        product_name: productName,
-        product_type: productType,
-        quantity: quantity,
-        price: price,
-        latitude: latitude,
-        longitude: longitude
-    };
+const getDataFormMyForm = () => {
+  getPromiseProductDataForRequest()
+    .then(result => {
+      return promiseForSanitizeData(result);
+    })
+    .then(result => {
+      return promiseBuildData(result);
+    })
+    .then(result => {
+      console.log(result); 
+    });
 };
+
+
+
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
     'use strict'
@@ -54,6 +102,5 @@ const buildProductDataForRequest = () => {
         }, false)
       })
   })()
-
 
 
